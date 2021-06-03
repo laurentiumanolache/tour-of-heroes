6ddb236622e55622c5dd17c193207dcd255769d1
Laurentiu Manolache <laurentiu.manolache18@gmail.com>
2021-06-03 20:13:36 +0300

    #2 add powerStats

--- angular-tour-of-heroes/src/app/hero.ts
+++ angular-tour-of-heroes/src/app/hero.ts
@@ -1,4 +1,11 @@
 export interface Hero {
     id: number;
     name: string;
+    powerStats: {
+        combat: number;
+        speed: number;
+        intelligence: number;
+        strength: number;
+        overallRating?: number;
+    }
 }
--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Dr Nice'},
-    { id: 12, name: 'Narco'},
-    { id: 13, name: 'Bombasto'},
-    { id: 14, name: 'Celeritas'},
-    { id: 15, name: 'Magneta'},
-    { id: 16, name: 'RubberMan'},
-    { id: 17, name: 'Dynama'},
-    { id: 18, name: 'Dr IQ'},
-    { id: 19, name: 'Magma'},
-    { id: 20, name: 'Tornado'}
+    { id: 11, name: 'Dr Nice', powerStats: { combat: 5, speed: 6, intelligence: 8, strength:5} },
+    { id: 12, name: 'Narco', powerStats: { combat: 7, speed: 8, intelligence: 5, strength:9} },
+    { id: 13, name: 'Bombasto', powerStats: { combat: 7, speed: 5, intelligence: 7, strength:6} },
+    { id: 14, name: 'Celeritas', powerStats: { combat: 4, speed: 8, intelligence: 6, strength:7} },
+    { id: 15, name: 'Magneta', powerStats: { combat: 6, speed: 5, intelligence: 9, strength:9} },
+    { id: 16, name: 'RubberMan', powerStats: { combat: 9, speed: 6, intelligence: 5, strength:7} },
+    { id: 17, name: 'Dynama', powerStats: { combat: 4, speed: 8, intelligence: 6, strength:7} },
+    { id: 18, name: 'Dr IQ', powerStats: { combat: 8, speed: 7, intelligence: 5, strength:6} },
+    { id: 19, name: 'Magma', powerStats: { combat: 10, speed: 3, intelligence: 6, strength:5} },
+    { id: 20, name: 'Tornado', powerStats: { combat: 4, speed: 3, intelligence: 10, strength:8} }
 ];
